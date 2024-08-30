f0095ac79bbd1e280569f138e8a8d93ef7a63880
newplayer-afk <sdc.startup@gmail.com>
2024-08-29 21:32:13 -0400

    Complete object and object constructors lesson

---
+++ script.js
@@ -0,0 +1,29 @@
+function book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function(){
+        return this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read
+    }
+}
+
+const theHobbit = new book('The Hobbit','J.R.R. Tolkien', '295', 'not read');
+
+console.log(theHobbit.info());
+console.log(Object.getPrototypeOf(theHobbit) === book.prototype)
+
+/*The original object "theHobbit" inherits from the book prototype. 
+Therefore if I define a function on the prototype, theHobbit can access this function. */
+
+book.prototype.sayHello = function(){
+    return "Hello!"
+}
+
+console.log(theHobbit.sayHello())
+
+/*What the original is based off of. 
+We can use this to save memory or inherit properties from the prototype*/
+
+/* This goes to another level, book.prototype inherits from Object.prototype */
+
