b3deec5f5a45f4be21340f7342d1c963119af4a6
newplayer-afk <sdc.startup@gmail.com>
2024-08-31 18:03:59 -0400

    Add bookAddButton Add functionality to bookAddButton Add header and footer to index.html Add div.table to index.html to integrate flexbox horizontally Add styling to library app in style.css

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 //DEFINE TABLE, TABLE BODY
+const body = document.querySelector("body");
+const main = document.querySelector("main");
 const table = document.querySelector("table");
 const tbody = document.querySelector("tbody");
 //CREATE ROW
@@ -7,22 +9,20 @@ let data = document.createElement("td");
 //ADD ROW TO TBODY
 tbody.appendChild(row);
 
-
+//CREATE ASIDE
+let bookAside = document.createElement("aside");
 
 function book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    //this.info = function(){
-        //return this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read
-    //}
 }
 
 const theHobbit = new book('The Hobbit','J.R.R. Tolkien', '295', 'No');
 
 //console.log(theHobbit.info());
-console.log(Object.getPrototypeOf(theHobbit) === book.prototype);
+//console.log(Object.getPrototypeOf(theHobbit) === book.prototype);
 
 /*The original object "theHobbit" inherits from the book prototype. 
 Therefore if I define a function on the prototype, theHobbit can access this function. */
@@ -47,15 +47,20 @@ function addBookToLibrary(book) {
 
 
 addBookToLibrary(theHobbit)
-console.log(theHobbit)
+//console.log(theHobbit)
 
 for (const book in myLibrary){
     tbody.appendChild(row);
     var currentBook = myLibrary[book];
-    console.log(currentBook)
+    //console.log(currentBook)
     for (var info in currentBook) {
-        console.log(info)
-        console.log(currentBook[info])
+        //console.log(info)
+        //console.log(currentBook[info])
         row.insertCell().textContent = `${currentBook[info]}`;
     }
 }
+
+const bookAddButton = document.querySelector("button#bookAdd");
+bookAddButton.addEventListener("click", () => {
+   main.appendChild(bookAside) ;
+})
